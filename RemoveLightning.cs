52fc4485115c0fe3e4c365767be2de776b6b0760
benginerd <115326516+benginerd@users.noreply.github.com>
2022-10-07 18:43:55 -0500

    Add files via upload

---
+++ RemoveLightning.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+public class RemoveLightning : IModApi
+{
+    public void InitMod(Mod mod)
+    {
+        Debug.Log("Loading Remove Lightning Patch: " + GetType().ToString());
+        var harmony = new Harmony(GetType().ToString());
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    [HarmonyPatch(typeof(WeatherManager))]
+    [HarmonyPatch("FrameUpdate")]
+    class ByeThunder
+    {
+        static void Prefix()
+        {
+
+            FieldInfo FieldIsThunderLastTime = AccessTools.Field(typeof(WeatherManager), "thunderLastTime");
+            FieldIsThunderLastTime.SetValue(WeatherManager.Instance, 100000000000000f);
+
+        }
+    }
+}
